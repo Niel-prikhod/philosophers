a65859564ebd6e7eb28d4eaf02b678c3bf419e3b
Niel <dyprih@gmail.com>
2026-05-02 17:47:56 +0200

    add function to calculate timestamps

---
+++ inc/phl_utils.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phl_utils.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dprikhod <dprikhod@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/02 14:30:29 by dprikhod          #+#    #+#             */
+/*   Updated: 2026/05/02 17:47:10 by dprikhod         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHL_UTILS_H
+# define PHL_UTILS_H
+
+typedef unsigned long t_phl_time;
+
+t_phl_time phl_get_timestamp(t_phl_time begin);
+
+#endif
---
+++ srcs/phl_utils.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phl_utils.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dprikhod <dprikhod@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/02 13:36:19 by dprikhod          #+#    #+#             */
+/*   Updated: 2026/05/02 14:38:00 by dprikhod         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <sys/time.h>
+#include "philo.h"
+#include "phl_utils.h"
+
+t_phl_time phl_get_timestamp(t_phl_time begin)
+{
+	struct timeval	tv;
+	t_phl_time		current;
+
+	gettimeofday(&tv, NULL);
+	current = tv.tv_sec * 1000 + tv.tv_usec / 1000 - begin;
+	return	(current);
+}
